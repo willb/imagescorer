d1249dc85aab6abb67193984b55ab10fca6ddfa3
William Benton <willb@redhat.com>
2018-03-16 15:52:08 -0500

    updated main.py

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -7,12 +7,12 @@ from swagger_server import encoder
 from darkflow.net.build import TFNet
 
 def main():
-    options = {"model": "cfg/yolo.cfg", "load": "bin/yolo.weights", "threshold": 0.1}    
+    df_options = {"metaLoad": "model/yolo.meta", "pbLoad": "model/yolo.pb", "threshold": 0.1}    
     
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'YOLO Image Scorer'})
-    app.tfnet = TFNet(options)
+    app.tfnet = TFNet(df_options)
 
     app.run(port=8080)
 
